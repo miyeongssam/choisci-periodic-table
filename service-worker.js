1b29b7001fbe6bced54cb1a0892b185a8139269e
mychoi <mychoi713@gmail.com>
2026-05-25 20:33:39 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// 최과학 주기율표 PWA Service Worker
+const CACHE_NAME = 'choisci-pt-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 설치: 핵심 파일 캐시
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 활성화: 옛날 캐시 삭제
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 요청 처리: 네트워크 우선, 실패 시 캐시
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((r) => r || caches.match('./index.html')))
+  );
+});
